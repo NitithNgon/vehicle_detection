a96078c730bcebd4d1f7af453ba8b9d1f02ad61a
Nitith Ngonchaiyaphum <nitith88871@gmail.com>
2024-11-27 01:59:15 +0700

    train_data_file

--- traing_data.py
+++ traing_data.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="coco8.yaml", epochs=100, imgsz=640)
+results = model.train(data="config.yaml", epochs=100, imgsz=640)
