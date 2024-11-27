71be9c04acfe119f99339e781ae4e6385b0cfabd
Peiam_eei-01_MSI <nitith88871@gmail.com>
2024-11-27 20:07:40 +0700

    traing

--- traing_data.py
+++ traing_data.py
@@ -4,4 +4,7 @@ from ultralytics import YOLO
 model = YOLO("yolo11n.yaml")  # build a new model from YAML
 
 # Train the model
-results = model.train(data="config.yaml", epochs=100, imgsz=640)
+results = model.train(data="config.yaml", epochs=100, imgsz=640)  #, imgsz=640 (downscale)
+
+# Start training from a pretrained *.pt model
+# yolo detect train data=config.yaml model=yolo11n.pt epochs=100 imgsz=640
